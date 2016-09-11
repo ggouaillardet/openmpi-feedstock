5bceb5199692c9b63a65fa28040e1a347ca39923
Thomas Robitaille <thomas.robitaille@gmail.com>
2016-09-11 11:37:29 +0100

    Attempt to get Fortran bindings working

--- recipe/build.sh
+++ recipe/build.sh
@@ -3,19 +3,24 @@
 OPTS=""
 
 if [ "$(uname)" == "Darwin" ]; then
-    export DYLD_FALLBACK_LIBRARY_PATH=$PREFIX/lib
+    export DYLD_FALLBACK_LIBRARY_PATH="${PREFIX}/lib"
     export CC=clang
     export CXX=clang++
     export MACOSX_DEPLOYMENT_TARGET="10.9"
     export CXXFLAGS="-stdlib=libc++ $CXXFLAGS"
     export CXXFLAGS="$CXXFLAGS -stdlib=libc++"
-    OPTS="--disable-mpi-fortran"
+    export LIBRARY_PATH="${PREFIX}/lib"
+    export LD_LIBRARY_PATH="${PREFIX}/lib"
 fi
 
+ls $DYLD_FALLBACK_LIBRARY_PATH
+
+echo 'program test; end program test' > test.f90
+gfortran test.f90
+./a.out
 
 ./configure --prefix=$PREFIX \
-            --disable-dependency-tracking \
-            $OPTS
+            --disable-dependency-tracking
 
 
 make -j $CPU_COUNT
