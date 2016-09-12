c254a1f8a570ab89bdfaf0a190553d271c40e424
Thomas Robitaille <thomas.robitaille@gmail.com>
2016-09-12 16:11:58 +0100

    Don't set LD_LIBRARY_PATH

--- recipe/build.sh
+++ recipe/build.sh
@@ -3,21 +3,16 @@
 OPTS=""
 
 if [ "$(uname)" == "Darwin" ]; then
-    export DYLD_FALLBACK_LIBRARY_PATH="${PREFIX}/lib"
+    export DYLD_FALLBACK_LIBRARY_PATH=$PREFIX/lib
     export CC=clang
     export CXX=clang++
     export MACOSX_DEPLOYMENT_TARGET="10.9"
     export CXXFLAGS="-stdlib=libc++ $CXXFLAGS"
     export CXXFLAGS="$CXXFLAGS -stdlib=libc++"
     export LIBRARY_PATH="${PREFIX}/lib"
-    export LD_LIBRARY_PATH="${PREFIX}/lib"
+    # export LD_LIBRARY_PATH="${PREFIX}/lib"
 fi
 
-ls $DYLD_FALLBACK_LIBRARY_PATH
-
-echo 'program test; end program test' > test.f90
-gfortran test.f90
-./a.out
 
 ./configure --prefix=$PREFIX \
             --disable-dependency-tracking
